64c39f267d0f43e1397468ee66dbd6f2245d712e
KissaBalint <kissb1@kkszki.hu>
2025-09-25 07:56:46 +0200

    Feladat 1 is ready.

---
+++ Ciklusok_Feladat2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciklusok_Feladat2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Feladat1
+            Console.WriteLine("Első feladat:");
+            Console.Write("Kérem az első számot:");
+            int szam1 = int.Parse(Console.ReadLine());
+            Console.Write("Kérem az második számot:");
+            int szam2 = int.Parse(Console.ReadLine());
+
+           
+
+            if (szam1 > szam2)
+            {
+                Console.WriteLine($"A nagyobb szám a: {szam1} és a különbségük {szam1-szam2}");
+
+            }
+            else
+            {
+                Console.WriteLine($"A nagyobb szám a: {szam2} és a különbségük {szam2 - szam1}");
+            }
+
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
