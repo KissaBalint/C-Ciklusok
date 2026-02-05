415faaffde7a89a3f58166b622e1f3187aef767a
KissBa <kissb1@kkszki.hu>
2026-02-05 12:08:22 +0100

    adatbazisba iras kesz

--- TraderConsole/Program.cs
+++ TraderConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TraderConsole
 {
@@ -12,7 +13,21 @@ namespace TraderConsole
         public static string ConnectionString = "server=localhost;database=trader;user=root;password=";
         public static MySqlConnection Conn = new MySqlConnection(ConnectionString);
 
+        public static void InsertData(string brand, string type, string color)
+        {
+            Conn.Open();
+            string sql = "INSERT INTO `cars` (`Brand`,`Type`,`Color`) VALUES (@brand, @type, @password)";
 
+            MySqlCommand cmd = new MySqlCommand(sql,Conn);
+
+            cmd.Parameters.AddWithValue("@brand", brand);
+            cmd.Parameters.AddWithValue("@type", type);
+            cmd.Parameters.AddWithValue("@color", color);
+
+            cmd.ExecuteNonQuery();
+
+            Conn.Close();
+        }
 
         static void Main(string[] args)
         {
