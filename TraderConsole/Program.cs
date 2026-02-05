76a9779c0019c0951c40a91d486cde7087dddec1
KissBa <kissb1@kkszki.hu>
2026-02-05 11:52:43 +0100

    connection kesz

---
+++ TraderConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraderConsole
+{
+    internal class Program
+    {
+        public static string ConnectionString = "server=localhost;database=trader;user=root;password=";
+        public static MySqlConnection Conn = new MySqlConnection(ConnectionString);
+
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
